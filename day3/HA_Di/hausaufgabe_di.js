5acf4084bcb0fa650ddef6db35701478688f812b
Schubsvieh <franziskakuehne55.com>
2024-03-19 14:17:12 +0100

    ..

---
+++ day3/HA_Di/hausaufgabe_di.js
@@ -0,0 +1,17 @@
+var myarray = [1, 2, 3, 4, 5];
+
+
+
+
+
+function entferneElement(arr, wert) {
+ 
+    let position = myarray.indexOf(4);
+    console.log(position);
+    var deleted_element = myarray.slice(1,1);
+  }
+
+  console.log(entferneElement(myarray, 3));
+  
+
+
